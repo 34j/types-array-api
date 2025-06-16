a66fdceb4ec850af908c421044fc1a4508a115ba
34j <34j.github@proton.me>
2025-06-16 13:52:35 +0900

    fix: fix type stub

--- src/array_api_compat/__init__.pyi
+++ src/array_api_compat/__init__.pyi
@@ -1,9 +1,9 @@
 from typing import Any, Literal
 
-from array_api._2024_12 import ArrayNamespace
+from array_api._2024_12 import Array, ArrayNamespace
 
-def array_namespace(
-    *xs: Any | complex | None,
-    api_version: Literal["v2024.12"] | None = None,
+def array_namespace[TArray: Array](
+    *xs: TArray | complex | None,
+    api_version: Literal["2024.12"] | None = None,
     use_compat: bool | None = None,
-) -> ArrayNamespace: ...
+) -> ArrayNamespace[TArray, Any, Any, Any, Any, Any]: ...
