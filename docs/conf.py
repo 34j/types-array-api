241d8ad1f6a13e82096fd8bafe9d2d3ae3090dcd
34j <34j.github@proton.me>
2025-12-11 18:29:28 +0900

    docs: add autosummary

--- docs/conf.py
+++ docs/conf.py
@@ -27,8 +27,10 @@ extensions = [
     "myst_parser",
     "sphinx.ext.napoleon",
     "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
     "sphinx.ext.viewcode",
 ]
+autosummary_generate = True
 napoleon_google_docstring = False
 
 # The suffix of source filenames.
